7c8dccd175b9e81efb4dd9c6a577b426d39d8767
Zongze Wu <astrowu61@gmail.com>
2024-11-21 15:11:25 -0800

    connect ?

--- db.js
+++ db.js
@@ -4,7 +4,11 @@ import fs from 'fs';
 const { Pool } = pkg;
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
+    user: 'Wu',
+    host: 'gobang-db.cxaws4k40uln.us-east-2.rds.amazonaws.com',
+    database: 'gobang_db',
+    password: '20040622Wzz',
+    port: 5432,
     ssl: {
         ca: fs.readFileSync('./us-east-2-bundle.pem').toString(),
         rejectUnauthorized: true, 
@@ -13,10 +17,12 @@ const pool = new Pool({
 
 export default pool;
 
+// 测试数据库连接
 pool.query('SELECT NOW()', (err, res) => {
     if (err) {
         console.error('Database connection error:', err);
     } else {
-        console.log('Connected to the database at:', res.rows[0]);
+        console.log('Connected to the database at:', res.rows[0].now);
     }
 });
+
