46db5ec4050d2922e9aa7d27e0f280833fe8a2b6
Zongze Wu <astrowu61@gmail.com>
2024-11-21 11:28:19 -0800

    connect database on Render

--- db.js
+++ db.js
@@ -3,12 +3,18 @@ import pkg from 'pg';
 const { Pool } = pkg;
 
 const pool = new Pool({
-    user: 'Wu',        
-    host: 'gobang-db.cxaws4k40uln.us-east-2.rds.amazonaws.com',
-    database: 'gobang_db',            
-    password: '20040622Wzz',     
-    port: 5432,
-    ssl: { rejectUnauthorized: false }
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false, 
+    },
 });
 
 export default pool;
+
+pool.query('SELECT NOW()', (err, res) => {
+    if (err) {
+        console.error('Database connection error:', err);
+    } else {
+        console.log('Connected to the database at:', res.rows[0]);
+    }
+});
